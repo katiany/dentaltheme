0553b6f97263118a46a82ddbdff57ada085cb783
Katiany Zimmermann <katizim+git@gmail.com>
2014-07-22 20:32:41 -0400

    Comments added.

--- content-contentonly.php
+++ content-contentonly.php
@@ -1,23 +1,19 @@
 <?php
 /**
- * The template used for pages that display only content without title and comments.
+ * Template used for pages that display only content without title and comments.
  *
  * @package Dental
  */
 ?>
-
+<?php /*The function the_ID() displays the numeric ID of the current post while
+the function post_class() adds classes related to the div tag and prints it out. */ ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'dental' ),
-				'after'  => '</div>',
-			) );
-		?>
+		<?php the_content(); /*The function the_content() from WordPress displays the contents of the current post. */ ?>
 	</div><!-- .entry-content -->
 	<footer class="entry-footer">
+		<?php /* The function edit_post_link() displays a link to edit the current post (if the user is logged in and has permissions.) */ ?>
 		<?php edit_post_link( __( 'Edit', 'dental' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
--- page-content.php
+++ page-content.php
@@ -4,23 +4,29 @@
  * Description: Template for pages with content-only. No comments or title are displayed.
  */
 
+//Gets the file header.php from the theme's directory and includes it in the structure of the page.
 get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php
+			
+			/* The loop 'while' executes repeatedly while the query has posts to be shown. 
+			The method have_posts() checks if there are posts in the current WordPress query. */
+			while ( have_posts() ) :
+				//The method the_post() displays the post content in the page
+				the_post();
+				//Loads the template 'contentonly' into the current template.
+				get_template_part( 'content', 'contentonly' );
 
-				<?php get_template_part( 'content', 'contentonly' ); ?>
-
-			<?php endwhile; // end of the loop. ?>
+			endwhile; // end of the loop. ?>
 			
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
-
+<?php get_sidebar(); /* Gets the file sidebar.php from the theme's directory to include it in the structure of the page. */ ?>
+<?php get_footer(); /* Gets the file footer.php from the theme's directory to include it in the structure of the page */ ?>
 
 ?>
